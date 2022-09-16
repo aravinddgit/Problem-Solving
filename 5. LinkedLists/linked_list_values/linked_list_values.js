80c658b0aa8125b705a4ef491fb0d5375509738f
Aravind Dinakaran <aravinddmail@gmail.com>
2022-09-16 15:39:14 -0700

    Commenting sample tests

--- 5. LinkedLists/linked_list_values/linked_list_values.js
+++ 5. LinkedLists/linked_list_values/linked_list_values.js
@@ -1,19 +1,23 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
 
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
 
-a.next = b;
-b.next = c;
-c.next = d;
+// a.next = b;
+// b.next = c;
+// c.next = d;
 
+
+
+// Solution
+// ---------
 // Iterative
 // const linkedListValues = (head) => {
 //     arr = [];
