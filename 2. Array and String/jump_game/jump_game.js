aa4ced6b5f6e71f70c12f311289bb686b1426efe
Aravind Dinakaran <aravinddmail@gmail.com>
2022-09-19 13:32:48 -0700

    Uncommenting alternate solutions for better view.

--- 2. Array and String/jump_game/jump_game.js
+++ 2. Array and String/jump_game/jump_game.js
@@ -4,18 +4,18 @@
  */
 
 // Recursion - Backtracking
-//  var canJump = function (nums, position=0) {
-//     if(position === nums.length-1){
-//         return true;
-//     }
+ var canJump = function (nums, position=0) {
+    if(position === nums.length-1){
+        return true;
+    }
     
-//     let farthestJumpIndex = Math.min(position + nums[position],nums.length-1);
-//     for(let index=farthestJumpIndex; index>position; index--){
-//         return canJump(nums, index);
-//     }
+    let farthestJumpIndex = Math.min(position + nums[position],nums.length-1);
+    for(let index=farthestJumpIndex; index>position; index--){
+        return canJump(nums, index);
+    }
 
-//     return false;
-// }
+    return false;
+}
 
 // Complexity
 // Time: O(2^n) - There are 2^n(upper bound) ways of jumping from the first position to the last, where n is the length of array nums. 
@@ -24,29 +24,29 @@
 
 
 // Recursion - Dynamic Programming - Top-Down
-//  var canJumpFromPosition = (nums, position=0, memo) => {
-//     if(memo[position] !== undefined){
-//         return memo[position];
-//     }
+ var canJumpFromPosition = (nums, position=0, memo) => {
+    if(memo[position] !== undefined){
+        return memo[position];
+    }
     
-//     let farthestJumpIndex = Math.min(position + nums[position],nums.length-1);
-//     for(let index=farthestJumpIndex; index>position; index--){
-//         if(canJumpFromPosition(nums, index, memo)){
-//             memo[position] = true;
-//             return true;
-//         }
-//     }
+    let farthestJumpIndex = Math.min(position + nums[position],nums.length-1);
+    for(let index=farthestJumpIndex; index>position; index--){
+        if(canJumpFromPosition(nums, index, memo)){
+            memo[position] = true;
+            return true;
+        }
+    }
 
-//     memo[position] = false;
-//     return false;
-// }
+    memo[position] = false;
+    return false;
+}
 
 
-// var canJump = function(nums) {
-//     const memo = [];
-//     memo[nums.length-1] = true;
-//     return canJumpFromPosition(nums,0,memo);
-// };
+var canJump = function(nums) {
+    const memo = [];
+    memo[nums.length-1] = true;
+    return canJumpFromPosition(nums,0,memo);
+};
 
 // Complexity:
 // Time: O(n^2) -  For every element in the array, say i, we are looking at the next nums[i] elements to its right aiming to find a GOOD index. nums[i] can be at most n, where n is the length of array nums.
@@ -54,30 +54,30 @@
 
 
 // Dynamic Programming - Bottom-up
-// var canJump = function (nums) {
-//     let memo = [];
-//     memo[nums.length-1] = true;
+var canJump = function (nums) {
+    let memo = [];
+    memo[nums.length-1] = true;
 
-//     for(let index=nums.length-2; index>=0; index--){
-//         let farthestJumpIndex = Math.min(index + nums[index],nums.length-1);
-//         memo[index] = false;
-//         for(j=index+1; j<=farthestJumpIndex; j++){
-//             if(memo[j]){
-//                 memo[index] = true;
-//                 break;
-//             }
-//         }
-//     }
+    for(let index=nums.length-2; index>=0; index--){
+        let farthestJumpIndex = Math.min(index + nums[index],nums.length-1);
+        memo[index] = false;
+        for(j=index+1; j<=farthestJumpIndex; j++){
+            if(memo[j]){
+                memo[index] = true;
+                break;
+            }
+        }
+    }
 
-//     return memo[0];
-// }
+    return memo[0];
+}
 
 // Complexity
 // Time: O(n^2) - For every element in the array, say i, we are looking at the next nums[i] elements to its right aiming to find a GOOD index. nums[i] can be at most n, where n is the length of array nums.
 // Space: o(n) - This comes from the usage of the memo table.
 
 
-// Greedy solution
+// Greedy solution - BEST method
 var canJump = function (nums) {
     let lastGoodPos = nums.length - 1;
     for(let i=nums.length-2; i>=0; i--){
