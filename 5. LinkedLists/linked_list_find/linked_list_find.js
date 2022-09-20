1984aaf10b10242c55abc65c7a8b9195cf392a60
Aravind Dinakaran <aravinddmail@gmail.com>
2022-09-19 23:13:01 -0700

    Removing unnecessary check

--- 5. LinkedLists/linked_list_find/linked_list_find.js
+++ 5. LinkedLists/linked_list_find/linked_list_find.js
@@ -1,28 +1,26 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
 
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
 
-a.next = b;
-b.next = c;
-c.next = d;
+// a.next = b;
+// b.next = c;
+// c.next = d;
 
 // Iterative
 const linkedListFind = (head, target) => {
     while(head !== null){
-        if(head === null) return false;
         if(head.val === target) return true;
         head = head.next;
     }
     return false;
-
 }
 
 
