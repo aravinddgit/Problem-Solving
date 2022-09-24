66468db553e97602f05cb9620373d1d6636ed183
Aravind Dinakaran <aravinddmail@gmail.com>
2022-09-23 22:41:58 -1200

    Adding problem: breadth first values

---
+++ 6. Trees/breadth_first_values/breadth_first_values.js
@@ -0,0 +1,50 @@
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+
+
+
+const breadthFirstValues = (root) => {
+    if(root === null) return [];
+    let queue = [];
+    let bfsArr = [];
+    queue.push(root)
+    while(queue.length !== 0){
+        let parentNode = queue.shift();
+        bfsArr.push(parentNode.val);
+        if(parentNode.left) queue.push(parentNode.left);
+        if(parentNode.right) queue.push(parentNode.right);
+    }
+    return bfsArr;
+}
+
+
+let arr = breadthFirstValues(a); 
+//    -> ['a', 'b', 'c', 'd', 'e', 'f']
+
+console.log(`Final List: ${arr}`);
