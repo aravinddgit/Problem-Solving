e8b66b661f9d819570084af51b73349edeb12744
Aravind Dinakaran <aravinddmail@gmail.com>
2022-09-23 22:43:48 -1200

    Commenting test data

--- 6. Trees/breadth_first_values/breadth_first_values.js
+++ 6. Trees/breadth_first_values/breadth_first_values.js
@@ -1,24 +1,24 @@
 
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
 
 //      a
 //    /   \
@@ -44,7 +44,7 @@ const breadthFirstValues = (root) => {
 }
 
 
-let arr = breadthFirstValues(a); 
-//    -> ['a', 'b', 'c', 'd', 'e', 'f']
+// let arr = breadthFirstValues(a); 
+// //    -> ['a', 'b', 'c', 'd', 'e', 'f']
 
-console.log(`Final List: ${arr}`);
+// console.log(`Final List: ${arr}`);
