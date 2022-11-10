f74e069defae907f77799ed8d9d75fe159219179
Aravind Dinakaran <aravinddmail@gmail.com>
2022-11-10 15:00:26 -0700

    Adding comments, explanation and tests

--- 3. Miscellaneous/best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.js
+++ 3. Miscellaneous/best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.js
@@ -5,6 +5,8 @@
  var maxProfit = function(prices) {
     let minPrice = Infinity;
     let maxProfit = 0
+    // As we progress through the array the lowest price is updated into 'minPrice'
+    // and the maxProfit is calculated for every item in the array (Price of item - minPrice)
     for(let i=0; i<prices.length; i++){
         if(prices[i] < minPrice){
             minPrice = prices[i];
@@ -18,3 +20,7 @@
 // Complexity Analysis
 // Time: O(n) - Coz only 1 sweep through the array is sufficient
 // Space: O(1) - The number of variables remain the same even if input size increases
+
+
+console.log(maxProfit([7,1,5,3,6,4])) // 5
+console.log(maxProfit([7,6,4,3,1])) // 0
