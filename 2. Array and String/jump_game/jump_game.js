88fc9d8d6d4d833ff6032c656d57bc839b7809dd
Aravind Dinakaran <aravinddmail@gmail.com>
2022-11-30 19:04:56 -0700

    Adding comments

--- 2. Array and String/jump_game/jump_game.js
+++ 2. Array and String/jump_game/jump_game.js
@@ -4,6 +4,8 @@
  */
 
 // Recursion - Backtracking
+// Here, we are looping from the start of the array. For every index, we are finding a subarray which corresponds to the ones under the farthest jump from that index. Once we find an index in the subarray that could jump to the last index, we are going to the next position in the original array and doing the same.
+// This is the inefficient solution where we try every single jump pattern that takes us from the first position to the last. We start from the first position and jump to every index that is reachable. We repeat the process until last index is reached. When stuck, backtrack.
  var canJump = function (nums, position=0) {
     if(position === nums.length-1){
         return true;
@@ -24,6 +26,8 @@
 
 
 // Recursion - Dynamic Programming - Top-Down
+// Top-down Dynamic Programming can be thought of as optimized backtracking. It relies on the observation that once we determine that a certain index is good / bad, this result will never change. This means that we can store the result and not need to recompute it every time.
+// Therefore, for each position in the array, we remember whether the index is good or bad. Let's call this array memo and let its values be either one of: GOOD, BAD, UNKNOWN. This technique is called memoization[3].
  var canJumpFromPosition = (nums, position=0, memo) => {
     if(memo[position] !== undefined){
         return memo[position];
@@ -33,6 +37,7 @@
     for(let index=farthestJumpIndex; index>position; index--){
         if(canJumpFromPosition(nums, index, memo)){
             memo[position] = true;
+            memo[index] = true
             return true;
         }
     }
@@ -54,6 +59,10 @@ var canJump = function(nums) {
 
 
 // Dynamic Programming - Bottom-up
+// Similar to previous approach but this is iterative 
+// Top-down to bottom-up conversion is done by eliminating recursion. In practice, this achieves better performance as we no longer have the method stack overhead and might even benefit from some caching. More importantly, this step opens up possibilities for future optimization. The recursion is usually eliminated by trying to reverse the order of the steps from the top-down approach.
+// The observation to make here is that we only ever jump to the right. This means that if we start from the right of the array, every time we will query a position to our right, that position has already be determined as being GOOD or BAD. This means we don't need to recurse anymore, as we will always hit the memo table.
+
 var canJump = function (nums) {
     let memo = [];
     memo[nums.length-1] = true;
@@ -72,12 +81,15 @@ var canJump = function (nums) {
     return memo[0];
 }
 
+
 // Complexity
 // Time: O(n^2) - For every element in the array, say i, we are looking at the next nums[i] elements to its right aiming to find a GOOD index. nums[i] can be at most n, where n is the length of array nums.
 // Space: o(n) - This comes from the usage of the memo table.
 
 
 // Greedy solution - BEST method
+// Once we have our code in the bottom-up state, we can make one final, important observation. From a given position, when we try to see if we can jump to a GOOD position, we only ever use one - the first one (see the break statement). In other words, the left-most one. If we keep track of this left-most GOOD position as a separate variable, we can avoid searching for it in the array. Not only that, but we can stop using the array altogether.
+
 var canJump = function (nums) {
     let lastGoodPos = nums.length - 1;
     for(let i=nums.length-2; i>=0; i--){
