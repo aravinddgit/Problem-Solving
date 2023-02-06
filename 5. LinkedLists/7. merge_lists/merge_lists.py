9b81e11ec54626478d8fa71ef69b6f1e131ff3ed
Aravind Dinakaran <aravinddmail@gmail.com>
2023-02-06 12:41:05 -0700

    Adding other implementation of merge lists

--- 5. LinkedLists/7. merge_lists/merge_lists.py
+++ 5. LinkedLists/7. merge_lists/merge_lists.py
@@ -33,6 +33,7 @@ def print_list(head):
   output += str(head.val)
   print(output)
 
+# Iterative 1
 def merge_lists(head1, head2):
   if(head1 is None and head2 is None):
     return None
@@ -60,8 +61,51 @@ def merge_lists(head1, head2):
     
   return mergedHead
 
+# Iterative 2
+def merge_lists1(head1, head2):
+  dummyNode = Node(None)
+  current = dummyNode
+
+  while(head1 is not None and head2 is not None):
+    if(head1.val < head2.val):
+      current.next = head1
+      current = current.next
+      head1 = head1.next
+    else:
+      current.next = head2
+      current = current.next
+      head2 = head2.next
+  
+  if head1 is None:
+    current.next = head2
+  if head2 is None:
+    current.next = head1
+
+  return dummyNode.next
+
+
+# Recursive
+def merge_lists2(head1, head2):
+  if(head1 is None and head2 is None):
+    return None
+  if(head1 is None):
+    return head2
+  if(head2 is None):
+    return head1
+
+  if(head1.val < head2.val):
+    nextHead1 = head1.next
+    head1.next = merge_lists2(nextHead1, head2)
+    return head1
+  else:
+    nextHead2 = head2.next
+    head2.next = merge_lists2(head1, nextHead2)
+    return head2
+  
 
 print_list(a)
 print_list(q)
 print_list(merge_lists(a, q))
+# print_list(merge_lists1(a, q))
+# print_list(merge_lists2(a, q))
 # 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 12 -> 20 -> 25 -> 28 
